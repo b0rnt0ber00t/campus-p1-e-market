6d35cf2ff34f699a01a855a2a0f865db7e2151d9
b0rnt0ber00t <lanabackend@gmail.com>
2022-04-08 10:43:46 +0700

    update Models User(m,f,s)

---
+++ database/seeders/User/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders\User;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@e-market.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => str()->random(10),
+        ]);
+
+        User::create([
+            'name' => 'Customer',
+            'email' => 'customer@e-market.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => str()->random(10),
+        ]);
+    }
+}
